ced56d50b6a0f7771d2150864756f1e7c34d6190
Conan Bitter <conan.bitter@gmail.com>
2025-05-18 20:54:25 +0300

    Implement color data

---
+++ palcalc/src/colorcalc.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use image::RgbImage;
+
+pub struct ColorData(Vec<Vec<Vec<u64>>>);
+
+impl ColorData {
+    pub fn new() -> ColorData {
+        ColorData(vec![vec![vec![0u64; 256]; 256]; 256])
+    }
+
+    pub fn add(&mut self, image: &RgbImage) -> Result<()> {
+        for pixel in image.enumerate_pixels() {
+            let color = pixel.2;
+            self.0[color[0] as usize][color[1] as usize][color[2] as usize] += 1;
+        }
+        Ok(())
+    }
+}
